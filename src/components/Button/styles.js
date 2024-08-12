c43f61956e096aa04ca211c7b768dbf93f2cf11f
Felipe Marques <felipeonmarques@gmail.com>
2024-08-12 19:22:43 -0300

    finalizando a calculadora

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    margin: 2px;
+    border: 3px solid #000;
+    border-radius: 20px;
+    background-color: #00aaf0;
+    color: #ffffff;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover{
+        opacity:0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #aaaaff;
+    border: 5px solid #000;
+    border-radius: 20px;
+    margin: 20px 0 ;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #aaaaff;
+        border: 2px solid #000;
+        border-radius: 15px;
+
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #ffffff;
+    }
+`
